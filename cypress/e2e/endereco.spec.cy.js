1f207a3a3625f04ca55bc73a08e5ba757eace36f
Mateus Meneses <mateusmeneses86@gmail.com>
2022-12-22 15:32:42 -0300

    testando com page object e arquivo de dados

--- cypress/e2e/endereco.spec.cy.js
+++ cypress/e2e/endereco.spec.cy.js
@@ -1,14 +1,31 @@
 /// <reference types="cypress" />
-
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 describe('Funcionalidade Endereço faturamento e Endereço entrega', () => {
     beforeEach(() => {
         cy.visit('minha conta')
-        cy.fixture('perfil').then(dados =>{
+        cy.fixture('perfil').then(dados => {
             cy.login(dados.usuario, dados.senha)
         })
 
     });
     it('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Mateus', 'Meneses', 'Google', 'Brasil', 'av brasil', '234', 'São paulo', 'São Paulo', '62502-492', '4002-8922')
+    });
+
+    it.only('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,)
+
+
 
     });
 });
